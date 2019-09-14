bed967339106c39bb621af3908e88c88b6ac4e97
RobertoRamirez0 <ramirez.robertomu22@gmail.com>
2019-09-13 21:09:13 -0400

    pre form validation, added fields and set their default values,, values are controlled

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -7,6 +7,23 @@ const AnimalForm = (props) => {
     <div className="animal-form">
       <Form>
         <Field type="text" name="species" placeholder="Species" />
+        
+        <Field type="number" name="age" placeholder="Age" />
+        
+        <Field component="select" name="diet">
+          <option value="" disabled>Select Diet:</option> {/*<-- this is our placeholder*/}
+          <option value="carnivore">Carnivore</option>
+          <option value="herbivore">Herbivore</option>
+          <option value="omnivore">Omnivore</option>
+        </Field>
+        
+        <label>
+          <Field type="checkbox" name="vaccinations" />
+          <span>Vaccinations</span>
+        </label>
+
+        <Field component="textarea" name="notes" placeholder="Notes" />
+        
         <button type="submit">Submit</button>
       </Form>
     </div>
@@ -14,9 +31,16 @@ const AnimalForm = (props) => {
 }
 
 export default withFormik({
+  //values come from formik automatically === magic!
   mapPropsToValues: (values) => {
+    //this makes these inputs 'controlled'
     return {
-      species: values.species || 'def' 
+      //these keys line up with the 'names' attribute from our Fields
+      species: values.species || '' ,
+      age: values.age || '',
+      diet: values.diet || '',
+      vaccinations: values.vaccinations || false,
+      notes: values.notes || '' 
     }
   },
   handleSubmit: (values) => {
